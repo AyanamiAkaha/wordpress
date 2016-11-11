38f85767b15aa8ac6c8b700a65660305b35c3a29
Ayanami <ayanami@frater260.com>
2016-11-12 03:27:39 +0900

    created actual javascript code

--- smsapi-addons/lettercount.js
+++ smsapi-addons/lettercount.js
@@ -1,3 +1,28 @@
-document.onload=function(){
-	// TODO: find form element and number of recipients, and add character counting hook
+var counter={
+	counter: function(messageField, counterElement) {
+		// FIXME: implement actual counter
+		counterElement.textContent = messageField.value.length;
+	}
+
+	init: function(){
+		var messageField = document.getElementById('message');
+		if(messageField != null) {
+			var counterEl = document.createElement('P');
+			messageField.insertBefore(counter);
+			messageField.addEventListener(
+				'input',
+				function() {
+					this.counter(messageField, counterEl);
+				},
+				false
+			);
+		}
+	}
+}
+if(window.addEventListener) {
+	window.addEventListener('load', counter.init, false);
+} else if(window.attachEvent) {
+	window.attachEvent('onload', counter.init);
+} else {
+	console.error('unsupported browser!');
 }
