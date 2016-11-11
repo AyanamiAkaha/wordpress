b43221737644882c043b95d46473006c96602a71
Ayanami <ayanami@frater260.com>
2016-11-12 04:31:09 +0900

    fixed install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,17 @@ usage() {
 	echo 'and sets owner, group, and permissions.'
 	echo 'Requires defined WP_PLUGINS_DIR enviroment variable.'
 	echo 'example:'
-	echo "WP_PLUGINS_DIR=/var/www/hdtocs/wp-content/plugins/ ${0}"
+	echo "sudo WP_PLUGINS_DIR=/var/www/hdtocs/wp-content/plugins/ ${0}"
 }
 
 install_smsapi_addons() {
-	/usr/bin/install -o apache -g apache -m 775 -d "${WP_PLUGIN_DIR}/smsapi-addons"
-	/usr/bin/install -o apache -g apache -m 664 'smsapi-addons/smsapi-addons.php' "${WP_PLUGIN_DIR}/smsapi-addons/"
-	/usr/bin/install -o apache -g apache -m 664 'smsapi-addons/lettercount.js' "${WP_PLUGIN_DIR}/smsapi-addons/"
+	/usr/bin/install -o apache -g apache -m 775 -d "${WP_PLUGINS_DIR}/smsapi-addons"
+	/usr/bin/install -o apache -g apache -m 664 'smsapi-addons/smsapi-addons.php' "${WP_PLUGINS_DIR}/smsapi-addons/"
+	/usr/bin/install -o apache -g apache -m 664 'smsapi-addons/lettercount.js' "${WP_PLUGINS_DIR}/smsapi-addons/"
 }
 
-if [ "x${WP_PLUGIN_DIR}" == "x" ]; then
+if [ "x${WP_PLUGINS_DIR}" = 'x' ]; then
+	echo ${WP_PLUGINS_DIR}
 	usage
 	exit 1
 fi
