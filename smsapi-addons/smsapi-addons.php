28d0223a01bbacf4afe6340219277394a592e799
Ayanami <ayanami@frater260.com>
2016-11-12 03:48:46 +0900

    fixed paths

--- smsapi-addons/smsapi-addons.php
+++ smsapi-addons/smsapi-addons.php
@@ -6,7 +6,10 @@ Version:     1.0.0-dev
 Author:	     Ayanami <ayanami@frater260.com>
 */
 defined('ABSPATH') or die('No direct access please.');
-add_action('admin_menu', 'modify_smsapi_menu',999);
+define('SMSAPI_ADDONS_PATH',plugin_dir_path(__FILE__));
+define('SMSAPI_PLUGIN_PATH', plugins_url('newsletter-sms-smsapi'));
+
+require_once SMSAPI_PLUGIN_PATH . 'gateway.php';
 
 function modify_smsapi_menu() {
 	$menu = remove_submenu_page('smsapi-settings','smsapi-send-sms');
@@ -25,5 +28,7 @@ function smsapi_addons_gateway_routng() {
 	smsapi_gateway_routing();
 	enqueue_script('smsapi-addons-lettercount-js',SMSAPI_ADDONS_PATH."/lettercount.js");
 }
+
+add_action('admin_menu', 'modify_smsapi_menu',999);
 
 ?>
